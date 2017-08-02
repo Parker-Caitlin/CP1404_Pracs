4d76b014249eec8f9afc3a672fd4007d78789f9a
Parker-Caitlin <caitlin.parker@my.jcu.edu.au>
2017-08-03 09:49:37 +1000

    Add files via upload

---
+++ Prac_01/sales_bonus.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Practical
+Program to calculate and display a user's bonus based on sales.
+If sales are under $1,000, the user gets a 10% bonus.
+If sales are $1,000 or over, the bonus is 15%.
+"""
+sales = float(input("Enter Sales: $"))
+if sales >= 1000:
+    bonus = sales * 0.15
+    print("Bonus: ${:.2f}".format(bonus))
+else:
+    bonus = sales / 10
+    print("Bonus: ${:.2f}".format(bonus))
