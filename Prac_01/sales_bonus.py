3a77a22e2977188637f570ccc7569c60eb7677da
Parker-Caitlin <caitlin.parker@my.jcu.edu.au>
2017-08-03 10:35:30 +1000

    Add files via upload

--- Prac_01/sales_bonus.py
+++ Prac_01/sales_bonus.py
@@ -5,9 +5,12 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 sales = float(input("Enter Sales: $"))
-if sales >= 1000:
-    bonus = sales * 0.15
-    print("Bonus: ${:.2f}".format(bonus))
-else:
-    bonus = sales / 10
-    print("Bonus: ${:.2f}".format(bonus))
+while sales >= 0:
+    if sales >= 1000:
+        bonus = sales * 0.15
+        print("Bonus: ${:.2f}".format(bonus))
+    else:
+        bonus = sales / 10
+        print("Bonus: ${:.2f}".format(bonus))
+    sales = float(input("Enter Sales: $"))
+print("Invalid Sales")
