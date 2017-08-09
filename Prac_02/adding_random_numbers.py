dd18dc27e7ed3388bd1db9a898491085fb2c79ac
Parker-Caitlin <caitlin.parker@my.jcu.edu.au>
2017-08-10 09:31:56 +1000

    Add files via upload

---
+++ Prac_02/adding_random_numbers.py
@@ -0,0 +1,13 @@
+"""
+what did you see on line 1? 9
+what was the smallest number you could have seen, what was the largest? 5 and 20
+
+
+what did you see on line 2? 3
+what was the smallest number you could have seen, what was the largest? 4 and 8
+Could line 2 have produced a 4? yes as it doesn't include 3 to start
+
+what did you see on line 3? 2.9595166688705246
+what was the smallest number you could have seen, what was the largest? 2.5 and 5.5
+
+"""
